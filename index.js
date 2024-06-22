36f0efa1d20c1dbc1c8ae7a3d7ef8312df5167ee
Rachata <rachatakanit@gmail.com>
2024-06-22 22:51:23 +0700

    refactor

--- index.js
+++ index.js
@@ -6,12 +6,16 @@ let base_sol_select;
 let error_box;
 let confirm_button;
 
-const SOLVENTS = ["S", "W", "S/2"];
+const Solvents = Object.freeze({
+	S: Symbol("S"),
+	S_HALF: Symbol("S/2"),
+	W: Symbol("W"),
+});
 
 const BASE_SOLUTIONS = [
-	{ name: "D10S", dex: 10, solvent: "S" },
-	{ name: "D5S", dex: 5, solvent: "S" },
-	{ name: "50% glucose", dex: 50, solvent: "W" },
+	{ name: "D10S", dex: 10, solvent: Solvents.S },
+	{ name: "D5S", dex: 5, solvent: Solvents.S },
+	{ name: "50% glucose", dex: 50, solvent: Solvents.W },
 ];
 
 function showError(msg) {
@@ -25,7 +29,7 @@ function hideError() {
 	confirm_button.disabled = false;
 }
 
-function onDextroseChange() {
+function verifyAndGetBaseSolutions() {
 	const dex = Number(dex_num_input.value);
 	if (
 		dex_num_input.value.trim().length === 0 ||
@@ -37,28 +41,28 @@ function onDextroseChange() {
 	) {
 		showError("Invalid Dextrose value");
 		return;
-	} else hideError();
+	}
 
-	getBaseSolutions();
-}
-
-function getBaseSolutions() {
-	const dex = Number(dex_num_input.value);
 	const solvent = solvent_select.options[solvent_select.selectedIndex].text;
-
-	console.log(`Dex=${dex}, Solvent=${solvent}`);
 	let solutions = Array.from(BASE_SOLUTIONS)
-		.filter((s) => s.solvent === solvent && s.dex <= dex)
+		.filter((s) => s.solvent.description === solvent && s.dex <= dex)
 		.sort((lhs, rhs) => lhs.dex < rhs.dex);
+	if (solutions.length == 0) {
+		showError("Unable to find a base solution");
+		return;
+	}
 
-	if (solutions.length == 0) showError("Unable to find a base solution");
-	else hideError();
+	hideError();
 	return solutions;
 }
 
-function setBaseSolutions(solutions) {
-	console.log(JSON.stringify(solutions));
-	let options = solutions.map((sol) => {
+function reportAndShowBaseSolutions(base_solutions) {
+	const dex = Number(dex_num_input.value);
+	const solvent = solvent_select.options[solvent_select.selectedIndex].text;
+	desire_sol_confirm.innerText = `D${dex}${solvent}`;
+
+	console.log(JSON.stringify(base_solutions));
+	let options = base_solutions.map((sol) => {
 		let opt = document.createElement("option");
 		opt.value = sol.name;
 		opt.innerHTML = sol.name;
@@ -67,35 +71,34 @@ function setBaseSolutions(solutions) {
 	base_sol_select.replaceChildren(...options);
 }
 
-function reportDesiredSolution() {
-	const dex = Number(dex_num_input.value);
-	const solvent = solvent_select.options[solvent_select.selectedIndex].text;
-	desire_sol_confirm.innerText = `D${dex}${solvent}`;
+function resetBaseSolutions() {
+	let none_opt = document.createElement("option");
+	none_opt.innerHTML = "---";
+	none_opt.value = "none";
+	none_opt.disabled = true;
+	none_opt.selected = true;
+	base_sol_select.replaceChildren(none_opt);
 }
 
 function onConfirmOrEdit() {
 	if (confirm_button.innerText == "Confirm") {
-		let solutions = getBaseSolutions();
-		if (solutions.length == 0) return;
+		let sols = verifyAndGetBaseSolutions();
+		if (sols.length === 0) return;
 
 		confirm_button.innerText = "Edit";
 		desire_sol_select.classList.add("disabled");
 		desire_sol_confirm.classList.remove("disabled");
-		reportDesiredSolution();
-		setBaseSolutions(solutions);
+		reportAndShowBaseSolutions(sols);
 	} else {
 		confirm_button.innerText = "Confirm";
 		desire_sol_select.classList.remove("disabled");
 		desire_sol_confirm.classList.add("disabled");
-
-		setBaseSolutions([]);
+		resetBaseSolutions();
 	}
 }
 
 window.onload = () => {
-	base_sol_select = document.getElementById("base-sol-select");
-	dex_num_input = document.getElementById("d-input");
-	dex_num_input.addEventListener("change", onDextroseChange);
+	setUpDexInputs();
 	setUpSalineInputs();
 
 	desire_sol_select = document.getElementById("d-s-select");
@@ -105,16 +108,24 @@ window.onload = () => {
 	confirm_button.addEventListener("click", onConfirmOrEdit);
 
 	error_box = document.getElementById("error-box");
+
+	base_sol_select = document.getElementById("base-sol-select");
+	resetBaseSolutions();
 };
+
+function setUpDexInputs() {
+	dex_num_input = document.getElementById("d-input");
+	dex_num_input.addEventListener("change", verifyAndGetBaseSolutions);
+}
 
 function setUpSalineInputs() {
 	solvent_select = document.getElementById("s-input");
-	solvent_select.addEventListener("change", getBaseSolutions);
+	solvent_select.addEventListener("change", verifyAndGetBaseSolutions);
 	solvent_select.replaceChildren(
-		...SOLVENTS.map((s) => {
+		...Object.values(Solvents).map((s) => {
 			let opt = document.createElement("option");
-			opt.value = s;
-			opt.innerHTML = s;
+			opt.value = s.description;
+			opt.innerHTML = s.description;
 			return opt;
 		})
 	);
